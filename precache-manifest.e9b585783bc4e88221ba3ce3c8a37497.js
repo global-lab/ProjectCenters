43274fb7673005d284d7cb42a61734995752b62d
lokesh234 <lgangaramaney@wpi.edu>
2020-10-06 21:25:55 -0400

    Updates

---
+++ precache-manifest.e9b585783bc4e88221ba3ce3c8a37497.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98c89df73240a3b7a740f785bd2582d9",
+    "url": "/ProjectCenters/index.html"
+  },
+  {
+    "revision": "e76dcdc3602d89d5eda6",
+    "url": "/ProjectCenters/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "ae9c0f0287decf7efb7f",
+    "url": "/ProjectCenters/static/css/main.290d17ff.chunk.css"
+  },
+  {
+    "revision": "e76dcdc3602d89d5eda6",
+    "url": "/ProjectCenters/static/js/2.30b7caa9.chunk.js"
+  },
+  {
+    "revision": "0580f66cff7a7b2e7b5c118fd842bcfd",
+    "url": "/ProjectCenters/static/js/2.30b7caa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae9c0f0287decf7efb7f",
+    "url": "/ProjectCenters/static/js/main.2918fd4a.chunk.js"
+  },
+  {
+    "revision": "f1e78448395715648592",
+    "url": "/ProjectCenters/static/js/runtime-main.a2af1fa3.js"
+  },
+  {
+    "revision": "25bd2429d998d38e275efe65d5717ce7",
+    "url": "/ProjectCenters/static/media/WPI_Inst.25bd2429.png"
+  }
+]);
